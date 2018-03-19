5f4d004225a49b929a145b35e9771076256b4f63
edson.reis <edson.reis@prestador.com.br>
2018-03-19 13:38:41 -0300

    Tela login usuário.

--- app-chat/src/main/resources/static/scripts/controllers/login-controller.js
+++ app-chat/src/main/resources/static/scripts/controllers/login-controller.js
@@ -2,6 +2,7 @@
 
 angular.module('app-chat').controller('LoginController', function($scope, $http, $filter) {
 
+		$scope.variavel = "Testando..";
         $scope.login = function() {
         	var baseUrl = "api/chat/login";
         	$http.get(baseUrl).success(function(data) {
